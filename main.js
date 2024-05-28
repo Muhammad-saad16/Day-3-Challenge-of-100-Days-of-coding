c4d2521e4cd6dac900907d0bc50d90ae299da4c7
Saad <108891916+Muhammad-saad16@users.noreply.github.com>
2024-05-28 17:47:26 +0500

    Day-2-Challenge-of-100Days-of-coding
    Day-2-Challenge-of-100Days-of-coding

---
+++ main.js
@@ -0,0 +1,24 @@
+"use strict";
+//🚀 Day 3 Challenge: Start Coding! 🚀
+Object.defineProperty(exports, "__esModule", { value: true });
+//Question 7: Number Eight: Write addition, subtraction, multiplication, and division operations 
+//that each result in the number 8. Enclose your operations in print statements.
+let additionResult = 5 + 3;
+console.log("Addition Result:", additionResult);
+let subtractionResult = 10 - 2;
+console.log("Subtraction Result:", subtractionResult);
+let multiplicationResult = 4 * 2;
+console.log("Multiplication Result:", multiplicationResult);
+let divisionResult = 32 / 4;
+console.log("Division Result:", divisionResult);
+// Question 8: You should create four lines that look like this: console.log(5 + 3)
+// Your output should simply be four lines with the number 8 appearing once on each line.
+console.log(5 + 3); // Addition
+console.log(10 - 2); // Subtraction
+console.log(4 * 2); // Multiplication
+console.log(16 / 2); // Division
+//Question 9: Favorite Number: Store your favorite number in a variable. 
+//Then, create a message revealing your favorite number. Print that message.
+let favoriteNumber = 8;
+let message = `My favorite number is:${favoriteNumber}`;
+console.log(message);
---
+++ main.ts
@@ -0,0 +1,37 @@
+                               //🚀 Day 3 Challenge: Start Coding! 🚀
+
+//Question 7: Number Eight: Write addition, subtraction, multiplication, and division operations 
+//that each result in the number 8. Enclose your operations in print statements.
+
+
+let additionResult :number=  5+3
+console.log("Addition Result:", additionResult);
+
+
+let subtractionResult :number=  10-2
+console.log("Subtraction Result:", subtractionResult);
+
+let multiplicationResult :number=  4*2
+console.log("Multiplication Result:", multiplicationResult);
+
+let divisionResult :number=  32/4
+console.log("Division Result:", divisionResult);
+
+
+// Question 8: You should create four lines that look like this: console.log(5 + 3)
+// Your output should simply be four lines with the number 8 appearing once on each line.
+
+console.log(5 + 3); // Addition
+console.log(10 - 2); // Subtraction
+console.log(4 * 2); // Multiplication
+console.log(16 / 2); // Division
+
+
+
+//Question 9: Favorite Number: Store your favorite number in a variable. 
+//Then, create a message revealing your favorite number. Print that message.
+
+
+let favoriteNumber: number = 8;
+let message: string = `My favorite number is:${favoriteNumber}`;
+console.log(message);
